3bb996cdd03905e39b5d9b2f5a8010d9c2a76cd4
Cal-Rex <andy.gibb@healthcareskills.com>
2023-05-10 20:07:40 +0000

    add code into views.py to allow table data to be displayed on html, data displayed on html using django python template syntax (like flask)

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,23 @@
 # imports django shortcuts, the render function is what is used to render html templates
 from django.shortcuts import render
-
+# imports Item class from models.py, 
+# starting the library your pulling from with a "."
+# indicates it it found locally
+from .models import Item
 
 # Create your views here.
 # once a function to call the app has been created, 
 # be sure to add the app to the INSTALLED_APPS[] variable inside settings.py, 
 # in the main django_todo folder 
 def get_todo_list(request):
-    return render(request, "todo/todo_list.html")
+    # in the imported class "Item", find objects, all of them
+    items = Item.objects.all()
+    # now the items variable has been established
+    # the variable "context" is established as a dictionary
+    # the key can be the name of whatever, but the value is the "items" variable
+    context = {
+        'items': items
+    }
+    # the context variable is passed into the the render return so now it is being
+    # parsed into the todo_list.html template where the data can be accessed by a user
+    return render(request, "todo/todo_list.html", context)
