4a3ab7c2923dc56de1a3d7fabff6d72f8d51eb94
Cal-Rex <andy.gibb@healthcareskills.com>
2023-05-12 07:23:53 +0000

    link up form to database

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 # imports django shortcuts, the render function is what is used to render html templates
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # imports Item class from models.py, 
 # starting the library your pulling from with a "."
 # indicates it it found locally
@@ -22,6 +22,22 @@ def get_todo_list(request):
     # parsed into the todo_list.html template where the data can be accessed by a user
     return render(request, "todo/todo_list.html", context)
 
-
+# for add item page
 def add_item(request):
+    # if statement used for when form is submitted and page reloads.
+    # when the form is POSTed, this following function commands run
+    if request.method == "POST":
+        # ___________________________________
+        # variable names used here match the variable names clarified in models.py
+        # under the class "Item"
+        # ___________________________________
+        # pulls the value of the form field with the name attribute "item_name"
+        name = request.POST.get('item_name')
+        # pulls the boolean value of the form field with the name "done"
+        done = 'done' in request.POST
+        # next command creates an object out of the form data, 
+        # and marries the values up with the variable names listed in the class
+        Item.objects.create(name=name, done=done)
+
+        return redirect('get_todo_list')
     return render(request, "todo/add_item.html")
