a336ac7c328ddd545ed3bc556ff8cb8aac891e6e
Cal-Rex <andy.gibb@healthcareskills.com>
2023-05-14 08:24:50 +0000

    add files to begin testing

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
+# initial lesson on how to create tests
 # Create your tests here.
+# class TestDjango(TestCase):
+
+#     def test_working(self):
+#         self.assertEqual(1, 1)
+
+#     def test_working2(self):
+#         self.assertEqual(1, 0)
+    
+#     def test_working3(self):
+#         self.assertEqual(1, )
+    
+#     def test_working4(self):
+#         self.assertEqual(1, 0)
+       
+    
